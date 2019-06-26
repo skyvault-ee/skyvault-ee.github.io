501acf3492650ea20c3146643bf814a112abdded
Alexander Sadovsky <alino.sadovsky@gmail.com>
2019-06-26 22:08:01 +0200

    improve UI

--- js/send_cv.js
+++ js/send_cv.js
@@ -47,6 +47,7 @@ $(function() {
             if (firstName.indexOf(' ') >= 0) {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
+            $('#submit').attr('disabled', 'disabled');
             $.ajax({
                 url: "https://skyvault.dopice.sk/send_cv.php",
                 type: "POST",
@@ -60,12 +61,11 @@ $(function() {
                     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
                     $('#success > .alert-success')
-                        .append("<strong>Your message has been sent. </strong>");
+                        .append("<strong>Your CV has been sent. Thank you " + firstName + "! </strong>");
                     $('#success > .alert-success')
                         .append('</div>');
 
-                    //clear all fields
-                    $('#contactForm').trigger("reset");
+                    $('#submit').hide(100);
                 },
                 error: function() {
                     // Fail message
@@ -74,8 +74,8 @@ $(function() {
                         .append("</button>");
                     $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
                     $('#success > .alert-danger').append('</div>');
-                    //clear all fields
-                    $('#contactForm').trigger("reset");
+
+                    $('#submit').removeAttr('disabled');
                 },
             })
         },
